bdcb15fa1bb966c558ad7af0948e5885a8845dff
Atharva1918 <103655090+Atharva1918@users.noreply.github.com>
2022-04-15 16:54:07 +0530

    Add files via upload

---
+++ Image2.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import image3 from '../logo.png.png'
+
+export default function image2() {
+  return (
+    <>
+    <img src={image3} className="LOGO" alt="..." height="80px" width="80px"></img>
+
+    </>
+  )
+}
